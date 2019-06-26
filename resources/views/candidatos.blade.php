9e3af63348510fa7f1e74eefdad9eac7535ee0eb
Roberto <roberto.ht2ml@gmail.com>
2019-06-26 17:16:43 -0300

    corrigir links dos carroceis

--- resources/views/candidatos.blade.php
+++ resources/views/candidatos.blade.php
@@ -37,19 +37,46 @@
               <td> {{$candidato->nome}} </td>
               <td> {{$candidato->sobrenome}} </td>
               <td>
-                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                     <div class="carousel-inner">
-                         <div class="carousel-item active">
-                             <img class="d-block w-100" src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" alt="First slide" class="img-thumbnail">
-                         </div>
-                         <div class="carousel-item">
-                             <img class="d-block w-100" src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" alt="Second slide" class="img-thumbnail">
-                         </div>
-                         <div class="carousel-item">
-                             <img class="d-block w-100" src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" alt="Third slide" class="img-thumbnail">
-                         </div>
-                     </div>
-                 </div>
+                  <div class="bd-example">
+                      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active bg-dark text-white"></li>
+                              <li data-target="#carouselExampleCaptions" data-slide-to="1" class="bg-dark text-white"></li>
+                              <li data-target="#carouselExampleCaptions" data-slide-to="2" class="bg-dark text-white"></li>
+                          </ol>
+                          <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                  <img src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" class="d-block w-100" alt="...">
+                                  <div class="carousel-caption d-none d-md-block">
+                                      <h5 class="text-dark">First slide label</h5>
+                                      <p class="text-dark">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <img src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" class="d-block w-100" alt="...">
+                                  <div class="carousel-caption d-none d-md-block">
+                                      <h5 class="text-dark">Second slide label</h5>
+                                      <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <img src="{{ asset('storage/'.$candidato->portifolio_photo_1) }}" class="d-block w-100" alt="...">
+                                  <div class="carousel-caption d-none d-md-block">
+                                      <h5 class="text-dark">Third slide label</h5>
+                                      <p class="text-dark">Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev" style="background-color: black">
+                              <span class="carousel-control-prev-icon text-primary" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next" style="background-color: black">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                          </a>
+                      </div>
+                  </div>
               </td>
 
               <td> {{$candidato->created_at}} </td>
