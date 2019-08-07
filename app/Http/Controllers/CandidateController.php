2678cbc3af419e44e2637dfbbdaf45ea5594f5bd
Roberto <roberto.ht2ml@gmail.com>
2019-08-07 16:52:58 -0300

    Tratando obrigatoriedade do envio da foto no cadastro

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -19,29 +19,33 @@ class CandidateController extends Controller
         $usertype = $user->usertype;
 
         //Armazenando envio da primeira foto para o portifólio do candidato
-        $file = $request->file('photo');
-        $fileTypeContent = $file->extension();
 
-        //Array de tipos de arquivos válidos
-        $typesValid = array('jpeg','jpg');
 
-        //Validação do formato do arquivo recebido
-        if(in_array($fileTypeContent,$typesValid))
-        {
-            if ($request->file('photo')->isValid()) {
-                $path = $request->photo->store('images',['disk'=>'public']);
+        if($request->hasFile('photo')){
+            $file = $request->file('photo');
+            $fileTypeContent = $file->extension();
 
+            //Array de tipos de arquivos válidos
+            $typesValid = array('jpeg','jpg');
+
+            //Validação do formato do arquivo recebido
+            if(in_array($fileTypeContent,$typesValid))
+            {
+                if ($request->file('photo')->isValid()) {
+                    $path = $request->photo->store('images',['disk'=>'public']);
+
+                }
+
+            }else{
+
+                dd( "No!Unvalidated File");
             }
-
-        }else{
-
-            dd( "No!Unvalidated File");
         }
 
         //Armazenando novos dados no model
         $usertype->name=$request->get('nome');
         $usertype->last_name=$request->get('sobrenome');
-        $usertype->photo=$path;
+        @$usertype->photo=$path;
 
         //Salvando dados
         $usertype->save();
